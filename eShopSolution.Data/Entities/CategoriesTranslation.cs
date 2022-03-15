c92ecd6cf6a8a8eda381985370c24f16f8c4c3c2
A41056 PHẠM QUANG GIÁP <a41056@thanglong.edu.vn>
2022-03-15 21:57:38 +0700

    Database Design

---
+++ eShopSolution.Data/Entities/CategoriesTranslation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class CategoriesTranslation
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTitle { get; set; }
+        public string LangueId { get; set; }
+        public string SeoAlias { get; set; }
+    }
+}
